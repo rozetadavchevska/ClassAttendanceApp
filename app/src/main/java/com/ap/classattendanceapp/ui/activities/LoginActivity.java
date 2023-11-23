755835e6bdda3417e89d2429d3b3de7c9d64fa44
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-11-23 20:55:30 +0100

    added firebase authentication

--- app/src/main/java/com/ap/classattendanceapp/ui/activities/LoginActivity.java
+++ app/src/main/java/com/ap/classattendanceapp/ui/activities/LoginActivity.java
@@ -2,15 +2,45 @@ package com.ap.classattendanceapp.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ap.classattendanceapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.emailLogin);
+        password = findViewById(R.id.passwordLogin);
+        Button loginBtn = findViewById(R.id.loginBtn);
+
+        auth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(view -> {
+            String emailText = email.getText().toString();
+            String passwordText = password.getText().toString();
+            loginUser(emailText, passwordText);
+        });
+    }
+
+    private void loginUser(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
+            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(LoginActivity.this, StudentActivity.class));
+            finish();
+        });
     }
 }
