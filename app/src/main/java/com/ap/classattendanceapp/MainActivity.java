d04a9695de5a493d2faace8143948f1a9c8de6ab
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-11-23 16:34:58 +0100

    added Splash screen and Login screen

--- app/src/main/java/com/ap/classattendanceapp/MainActivity.java
+++ app/src/main/java/com/ap/classattendanceapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.ap.classattendanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.ap.classattendanceapp.ui.activities.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
     }
 }
---
+++ app/src/main/java/com/ap/classattendanceapp/ui/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ap.classattendanceapp.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ap.classattendanceapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
