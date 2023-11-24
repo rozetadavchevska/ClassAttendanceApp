494cc7a43a42eb3868d81902c21e8ecd2e88ed5b
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-11-24 19:22:46 +0100

    added screen for teacher

--- app/src/main/java/com/ap/classattendanceapp/ui/activities/TeacherActivity.java
+++ app/src/main/java/com/ap/classattendanceapp/ui/activities/TeacherActivity.java
@@ -5,48 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ap.classattendanceapp.R;
-import com.ap.classattendanceapp.ui.fragments.CalendarFragment;
+import com.ap.classattendanceapp.ui.fragments.AttendanceFragment;
 import com.ap.classattendanceapp.ui.fragments.ClassesFragment;
 import com.ap.classattendanceapp.ui.fragments.CoursesFragment;
 import com.ap.classattendanceapp.ui.fragments.HomeFragment;
 import com.ap.classattendanceapp.ui.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class StudentActivity extends AppCompatActivity {
+public class TeacherActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
     HomeFragment homeFragment = new HomeFragment();
     CoursesFragment coursesFragment = new CoursesFragment();
     ClassesFragment classesFragment = new ClassesFragment();
-    CalendarFragment calendarFragment = new CalendarFragment();
+    AttendanceFragment attendanceFragment = new AttendanceFragment();
     ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student);
+        setContentView(R.layout.activity_teacher);
 
-        bottomNav = findViewById(R.id.studentBottomNav);
+        bottomNav = findViewById(R.id.teacherBottomNav);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.studentFrameLayout, homeFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.teacherFrameLayout, homeFragment).commit();
 
         bottomNav.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
 
             if(itemId == R.id.home) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.studentFrameLayout, homeFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.teacherFrameLayout, homeFragment).commit();
                 return true;
             }else if(itemId == R.id.courses){
-                getSupportFragmentManager().beginTransaction().replace(R.id.studentFrameLayout, coursesFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.teacherFrameLayout, coursesFragment).commit();
                 return true;
             } else if(itemId == R.id.classes){
-                getSupportFragmentManager().beginTransaction().replace(R.id.studentFrameLayout, classesFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.teacherFrameLayout, classesFragment).commit();
                 return true;
-            } else if(itemId == R.id.calendar){
-                getSupportFragmentManager().beginTransaction().replace(R.id.studentFrameLayout, calendarFragment).commit();
+            } else if(itemId == R.id.attendance){
+                getSupportFragmentManager().beginTransaction().replace(R.id.teacherFrameLayout, attendanceFragment).commit();
                 return true;
             } else if(itemId == R.id.profile){
-                getSupportFragmentManager().beginTransaction().replace(R.id.studentFrameLayout, profileFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.teacherFrameLayout, profileFragment).commit();
                 return true;
             }
 
